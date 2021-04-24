db49382edc7e63999c1fd1d140e44dbf095d84bf
Alexander <alexanderwatersawc@gmail.com>
2021-04-24 13:59:45 -0400

    debugging pass by refrence

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def start_reciving(Arduino, mypet):
             mypet.hunger = 100
             mypet.tiereness = 100
             mypet.attention = 100
-            print("EPIC")
-        print("LOOP:", data)
+            print("feelgood")
+            print("From reciving", mypet.get_state())
 
 def start_sending(Arduino, mypet):
     last_state = ""
@@ -38,15 +38,17 @@ def start_sending(Arduino, mypet):
         if mypet.get_state() != "":
             Arduino.LCD_print(mypet.get_state())
             Arduino.write_to("State:Triggered")
+            print("From Sending", mypet.get_state())
             last_state = mypet.get_state()
         else:
             Arduino.LCD_clear()
             if last_state != "":
+                print("Not TRIGGERD")
+                print("From Sending", mypet.get_state())
                 Arduino.write_to("State:NotTriggered")
                 last_state=""
         
         time.sleep(1)
-        print(mypet.get_state())
 
 
 if __name__ == "__main__":
